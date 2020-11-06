25ae75e868f13fd279dc90571fa634c919b4fccc
u2017049 <73095318+u2017049@users.noreply.github.com>
2020-11-06 11:28:01 +0500

    Update App.js

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
             Ahmed Shamas!
-        Good Scene ha!!!!
+          Working!
         </p>
         <a
           className="App-link"
